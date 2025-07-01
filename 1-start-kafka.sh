be6f799554889dd2695a220313548146322a2ad2
jinseoplee <jjjp9966@gmail.com>
2025-07-01 18:06:13 +0900

    feat: Add Zookeeper and Kafka startup script

---
+++ 1-start-kafka.sh
@@ -0,0 +1,5 @@
+# Start Zookeeper in the background
+zookeeper-server-start.sh ~/kafka_2.13-3.1.0/config/zookeeper.properties
+
+# Start Kafka in the background
+kafka-server-start.sh ~/kafka_2.13-3.1.0/config/server.properties
